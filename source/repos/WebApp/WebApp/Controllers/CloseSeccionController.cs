91363a6a38cac74991560e10b5febb764fc6d72d
Abdias <felizseguraabdias@gmail.com>
2025-10-11 16:31:45 -0400

    Working on it

---
+++ source/repos/WebApp/WebApp/Controllers/CloseSeccionController.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class CloseSeccionController : Controller
+    {
+        // GET: CloseSeccion
+        public ActionResult LogOff()
+        {
+
+            Session["Usuario"] = null;
+            return RedirectToAction("Login", "Access");
+
+        }
+    }
+}
---
+++ source/repos/WebApp/WebApp/Models/ViewModels/QueryViewModels.cs
@@ -0,0 +1,67 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models.ViewModels
+{
+    public class QueryViewModels
+    { 
+        public int _Id { get; set; }    
+        public string _Email { get; set; }
+        public int? _Edad {  get; set; }
+
+    }
+
+    public class AddUserViewModel
+    {
+        [Required]
+        [Display(Name = "Nombre Usuario")]
+        public string _Nombre { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        public string _Clave { get; set; }
+        [Required]
+        [Display(Name = "Confirmar Password")]
+        [DataType(DataType.Password)]
+        [Compare("_Clave", ErrorMessage = "The password and confirmation password do not match.")]
+        public string _ClaveConfirma { get; set; }
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        [StringLength(100, ErrorMessage = "Inser your Email", MinimumLength = 6)]
+        public string _Email { get; set; }
+        [Required]
+        [Display(Name = "Edad")]
+        public int _Edad { get; set; }
+    }
+
+    public class EditUserViewModel
+    {
+        public int _Id { get; set; }
+
+
+        [Required]
+        [Display(Name = "Nombre Usuario")]
+        public string _Nombre { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        public string _Clave { get; set; }
+        [Required]
+        [Display(Name = "Confirmar Password")]
+        [DataType(DataType.Password)]
+        [Compare("_Clave", ErrorMessage = "The password and confirmation password do not match.")]
+        public string _ClaveConfirma { get; set; }
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        [StringLength(100, ErrorMessage = "Inser your Email", MinimumLength = 6)]
+        public string _Email { get; set; }
+        [Required]
+        [Display(Name = "Edad")]
+        public int? _Edad { get; set; }
+    }
+
+
+}
